6c3941f4f8f67ffa56cdc9afb6baa0749aad045c
catalinpatularu78 <A00304504@student.ait.ie>
2023-04-12 15:23:52 +0100

    chnaged war deploy

--- src/main/java/com/myproject/ship/ShipSpringApplication.java
+++ src/main/java/com/myproject/ship/ShipSpringApplication.java
@@ -2,9 +2,16 @@ package com.myproject.ship;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ShipSpringApplication {
+public class ShipSpringApplication extends SpringBootServletInitializer {
+
+	@Override
+	public SpringApplicationBuilder configure(SpringApplicationBuilder app) {
+		return app.sources(ShipSpringApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShipSpringApplication.class, args);
