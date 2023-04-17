eeaab5159ba5faf9180370f3764f8cb698eaf680
catalinpatularu78 <A00304504@student.ait.ie>
2023-04-17 12:19:15 +0100

    demo2

--- src/main/java/com/myproject/ship/ShipSpringApplication.java
+++ src/main/java/com/myproject/ship/ShipSpringApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShipSpringApplication {
+
 	public static void main(String[] args) {
 
 		SpringApplication.run(ShipSpringApplication.class, args);
